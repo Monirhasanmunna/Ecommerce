735aebf53f65448b92c757de9cf6fb53354a47f4
munna <monirhasanmunna1@gmail.com>
2022-06-13 14:03:27 +0600

    layouts create,contact,cart,delivery,home,productView and specialsOffer page implement

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -10,4 +10,29 @@ class BaseController extends Controller
     {
         return view('frontend.home');
     }
+
+    public function specialsOffer()
+    {
+        return view('frontend.specialsOffer');
+    }
+
+    public function delivery()
+    {
+        return view('frontend.delivery');
+    }
+
+    public function contact()
+    {
+        return view('frontend.contact');
+    }
+
+    public function cart()
+    {
+        return view('frontend.cart');
+    }
+
+    public function productView()
+    {
+        return view('frontend.productView');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[BaseController::class,'home'])->name('home');
+Route::get('/specialsOffer',[BaseController::class,'specialsOffer'])->name('specialsOffer');
+Route::get('/delivery',[BaseController::class,'delivery'])->name('delivery');
+Route::get('/contact_us',[BaseController::class,'contact'])->name('contact');
+Route::get('/cart',[BaseController::class,'cart'])->name('cart');
+Route::get('/product_details',[BaseController::class,'productView'])->name('product_details');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
